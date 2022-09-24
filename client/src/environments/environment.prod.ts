88e8999933e57de93165742e4f6e07f9352ffd6d
Tony Wu <twuu1992@gmail.com>
2022-09-24 15:07:31 +1000

    Update the api url; Update the nginx setting;

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:4000'
+  apiUrl: 'http://nlb-user-28bf4092c2dbb889.elb.ap-southeast-2.amazonaws.com/api' //TODO: Replace in the CI/CD next
 };
